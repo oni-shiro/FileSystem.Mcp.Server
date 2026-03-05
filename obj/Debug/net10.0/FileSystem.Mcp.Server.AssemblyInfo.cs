9bfe5570478023bde07aa688998b30217c43ba10
oni-shiro <sinchan.saha99@gmail.com>
2026-03-05 21:40:54 +0530

    2 rule based redaction added in core engine

--- obj/Debug/net10.0/FileSystem.Mcp.Server.AssemblyInfo.cs
+++ obj/Debug/net10.0/FileSystem.Mcp.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileSystem.Mcp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36860ea6c7d9ce6d02e6c17e9970b9118594055e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileSystem.Mcp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileSystem.Mcp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
